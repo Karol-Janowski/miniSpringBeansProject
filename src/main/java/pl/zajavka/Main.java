2fd77a58f905159f0043c3dbf2b44e85cec68010
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-12-13 12:49:29 +0100

    added some service and domain classes to project

---
+++ src/main/java/pl/zajavka/Main.java
@@ -0,0 +1,24 @@
+package pl.zajavka;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pl.zajavka.configuration.ApplicationConfiguration;
+import pl.zajavka.service.MainProcessor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+        MainProcessor mainProcessor = context.getBean(MainProcessor.class);
+
+//        MainProcessor mainProcessor = new MainProcessor(
+//                new DataRepository(),
+//                new DataProcessingService(
+//                        new StringPreparationService()
+//                )
+//        );
+        mainProcessor.process();
+    }
+
+}
